c0ae7465921119cf8c292a7be6035cf4dde557c7
Artem-Pronenko <pronenko.artem@gmail.com>
2020-08-11 17:08:10 +0300

    lesson 3

--- js/index.js
+++ js/index.js
@@ -22,9 +22,34 @@ console.log(`Период равен ${period} месяцев`)
 console.log(`Цель заработать ${mission} рублей`)
 
 console.log(addExpenses.toLowerCase().trim().split(' , '))
+// УРОК №3
+console.log('УРОК №3-------------------------------------------')
 
-let budgetDay = money / 30
-console.log(Math.floor(budgetDay * 100) / 100)
+money = +prompt('Ваш месячный доход?', '7000')
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', ' Лицензия на WebStorm , Микрофон, Интернет ')
+deposit = confirm('Есть ли у вас депозит в банке?')
 
+expenses1 = prompt('Введите обязательную статью расходов?', 'Интернет')
+amount1 = +prompt('Во сколько это обойдется?', '500')
 
+expenses2 = prompt('Введите обязательную статью расходов?', 'Интернет2')
+amount2 = +prompt('Во сколько это обойдется?', '500')
+
+let budgetMonth = money - (amount1 + amount2)
+console.log('Бюджет на месяц: ' + budgetMonth)
+let achieved = Math.ceil(mission / budgetMonth)
+console.log(`Цель будет достигнута за: ${achieved} месяцев`)
+
+budgetDay = Math.floor(budgetMonth / 30)
+console.log('Бюджет на день: ' + budgetDay)
+
+if (!budgetDay || budgetDay < 0) {
+  console.log('Что-то пошло не так')
+} else if (budgetDay >= 1200) {
+  console.log('У вас высокий уровень дохода')
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У вас средний уровень дохода')
+} else {
+  console.log('К сожалению у вас уровень дохода ниже среднего')
+}
 
